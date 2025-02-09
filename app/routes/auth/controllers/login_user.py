da466b30d63d8816a0f08284340a7ab392f4ec0e
arshkhatwani <arsh.khatwani@gmail.com>
2025-02-09 12:30:25 +0530

    Add feat to generate access token and create user if it does not exist

--- app/routes/auth/controllers/login_user.py
+++ app/routes/auth/controllers/login_user.py
@@ -1,6 +1,7 @@
 from app.routes.auth.models import LoginResponse, UserBody
 from app.services.db.postgres.database import get_db
 from app.services.db.postgres.models.user import User
+from app.services.jwt.service import jwt_service
 
 
 class LoginUser:
@@ -8,12 +9,19 @@ class LoginUser:
         self.user_body = user_body
 
     async def handle_request(self) -> LoginResponse:
-        if await self._check_user_exists():
-            return LoginResponse(access_token="test token")
+        user_exists = await self._check_user_exists()
 
-        return LoginResponse(access_token="test token")
+        user = user_exists if user_exists else await self._create_user()
+        access_token = jwt_service.encode_user_id(str(user.id))
 
-    async def _check_user_exists(self) -> bool:
+        return LoginResponse(access_token=access_token)
+
+    async def _check_user_exists(self) -> User | None:
         async with get_db() as db:
             result = await User.get_user_by_username(db, self.user_body.username)
-        return True if result else False
+        return result
+
+    async def _create_user(self) -> User:
+        async with get_db() as db:
+            result = await User.create(db, **self.user_body.model_dump())
+        return result
