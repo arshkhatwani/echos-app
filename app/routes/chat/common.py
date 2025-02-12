2fb6daf5ed4602d12239217c4d960f7e92175e08
arshkhatwani <arsh.khatwani@gmail.com>
2025-02-13 00:47:49 +0530

    Add logic to persist message in DB in case receiver is offline

--- app/routes/chat/common.py
+++ app/routes/chat/common.py
@@ -1,4 +1,7 @@
 from fastapi import WebSocket
+from app.services.db.postgres.database import get_db
+from app.services.db.postgres.models.pending_message import PendingMessage
+from app.routes.chat.constants import MessageType
 
 
 class UserConnectionManager:
@@ -19,10 +22,25 @@ class UserConnectionManager:
     ):
         receiver_socket = self.active_connections.get(receiver_id)
         if not receiver_socket:
+            await self.save_personal_message(
+                sender_id=sender_id, receiver_id=receiver_id, message=message
+            )
             return False
 
         await receiver_socket.send_json({"sender_id": sender_id, "message": message})
         return True
+
+    async def save_personal_message(
+        self, sender_id: str, receiver_id: str, message: str
+    ):
+        async with get_db() as db:
+            await PendingMessage.create(
+                db=db,
+                sender_id=sender_id,
+                receiver_id=receiver_id,
+                type=MessageType.SEND_MESSAGE,
+                message=message,
+            )
 
 
 user_connection_manager = UserConnectionManager()
