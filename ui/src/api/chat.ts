b905a98dd5ae2490321e11c40df45a4e5f28bea4
arshkhatwani <arsh.khatwani@gmail.com>
2025-02-21 18:47:30 +0530

    Add API call to search users for chat

---
+++ ui/src/api/chat.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { API_CONFIG } from "../config/constants";
+
+interface SearchUserResponse {
+  username: string;
+  user_id: string;
+}
+
+export const chat = {
+  async searchUsers(
+    search: string,
+    accessToken: string,
+  ): Promise<SearchUserResponse[]> {
+    const response = await axios.get<SearchUserResponse[]>(
+      `${API_CONFIG.SERVER_URL}${API_CONFIG.SEARCH_USERS_ENDPOINT}`,
+      {
+        params: {
+          search,
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    return response.data;
+  },
+};
