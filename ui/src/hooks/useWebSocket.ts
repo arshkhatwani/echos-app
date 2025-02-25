491d7c80dca66c0562d86e0738eb7d39fdf773bc
arshkhatwani <arsh.khatwani@gmail.com>
2025-02-25 16:10:10 +0530

    Implement WebSocket message sending logic in useWebSocket hook

--- ui/src/hooks/useWebSocket.ts
+++ ui/src/hooks/useWebSocket.ts
@@ -1,12 +1,17 @@
 import { useEffect, useRef } from "react";
 import { API_CONFIG } from "../config/constants";
 import { useAtom } from "jotai";
-import { accessTokenAtom, isAuthenticatedAtom } from "../store/atoms";
+import {
+  accessTokenAtom,
+  isAuthenticatedAtom,
+  sendMessageAtom,
+} from "../store/atoms";
 
 function useWebSocket() {
   const ws = useRef<WebSocket | null>(null);
   const [accessToken] = useAtom(accessTokenAtom);
   const [isAuthenticated] = useAtom(isAuthenticatedAtom);
+  const [sendMessage, setSendMessage] = useAtom(sendMessageAtom);
 
   useEffect(() => {
     if (!isAuthenticated || !accessToken) return;
@@ -28,6 +33,18 @@ function useWebSocket() {
       wsCurrent?.close();
     };
   }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (!sendMessage) return;
+
+    const wsCurrent = ws.current;
+    if (!wsCurrent) return;
+
+    wsCurrent.send(JSON.stringify(sendMessage));
+    console.log(sendMessage);
+
+    setSendMessage(null);
+  }, [sendMessage]);
 }
 
 export default useWebSocket;
