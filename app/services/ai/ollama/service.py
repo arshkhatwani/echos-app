be9aef9bd0c6dd40a419b3d21f58badbd73c8bb2
arshkhatwani <arsh.khatwani@gmail.com>
2025-03-22 18:54:08 +0530

    Refactor OllamaService to integrate XML parsing

--- app/services/ai/ollama/service.py
+++ app/services/ai/ollama/service.py
@@ -1,3 +1,5 @@
+import xmltodict
+
 from ollama import AsyncClient
 from app.services.ai.ollama.constants import TaskType
 
@@ -11,41 +13,50 @@ class OllamaService:
         result = await self.client.generate(model=self.model, prompt=prompt)
         return result.response
 
+    async def generate_and_parse_xml(self, prompt: str) -> str:
+        result = await self.generate(prompt)
+        return self.extract_xml_string(result)
+
     def get_prompt(self, task: TaskType, message: str) -> str:
         return f"""
         Task: {task}
         Message: {message}
         """
 
-    async def get_reply_suggestions(self, message: str) -> str:
+    async def get_reply_suggestions(self, message: str) -> list[str]:
         prompt = self.get_prompt(TaskType.REPLY_SUGGESTIONS, message)
-        response = await self.generate(prompt)
-        return response
+        response = await self.generate_and_parse_xml(prompt)
+        return xmltodict.parse(response).get("response", {}).get("reply", [])
 
     async def get_message_composition(self, prompt: str) -> str:
         prompt = self.get_prompt(TaskType.MESSAGE_COMPOSITION, prompt)
-        response = await self.generate(prompt)
+        response = await self.generate_and_parse_xml(prompt)
         return response
 
     async def get_text_completion(self, initial_phrase: str) -> str:
         prompt = self.get_prompt(TaskType.TEXT_COMPLETION, initial_phrase)
-        response = await self.generate(prompt)
+        response = await self.generate_and_parse_xml(prompt)
         return response
 
     async def get_rephrasing(self, original_text: str) -> str:
         prompt = self.get_prompt(TaskType.REPHRASING, original_text)
-        response = await self.generate(prompt)
+        response = await self.generate_and_parse_xml(prompt)
         return response
 
     async def get_summarization(self, original_text: str) -> str:
         prompt = self.get_prompt(TaskType.SUMMARIZATION, original_text)
-        response = await self.generate(prompt)
+        response = await self.generate_and_parse_xml(prompt)
         return response
 
     async def get_one_word_replies(self, original_text: str) -> str:
         prompt = self.get_prompt(TaskType.ONE_WORD_REPLIES, original_text)
-        response = await self.generate(prompt)
+        response = await self.generate_and_parse_xml(prompt)
         return response
+
+    def extract_xml_string(self, xml_string):
+        start_index = xml_string.find("```xml")
+        end_index = xml_string.find("```", start_index + 1)
+        return xml_string[start_index + 6 : end_index]
 
 
 ollama_service = OllamaService()
